2b755bd81f82e5f5f4b8d46a4c1267b20643b70b
WKavanagh-office <bkav93@gmail.com>
2018-08-31 14:20:39 +0100

    Educated done(?)

--- educate.py
+++ educate.py
@@ -1,1 +1,112 @@
-TODO
+import collections
+
+# Reads from info.txt and sets global values on players
+def read_player_info(deck1, deck2):
+    global p1_damage, p1_health, p1_deck, p2_damage, p2_health, p2_deck, info
+    p1_damage = None
+    p1_health = None
+    p1_deck = None
+    p2_damage = None
+    p2_health = None
+    p2_deck = None
+    info = file("info.txt","r").readlines()
+    deck1_string = "Deck_" + str(deck1)
+    deck2_string = "Deck_" + str(deck2)
+    for i in range(len(info)):
+        if deck1_string + "=[" in info[i]:
+            p1_deck = info[i].split("[")[1].split("]")[0]
+            p1_health = int(info[i+1].split("=")[1])
+            p1_damage = int(info[i+2].split("=")[1])
+        if deck2_string + "=[" in info[i]:
+            p2_deck = info[i].split("[")[1].split("]")[0]
+            p2_health = int(info[i+1].split("=")[1])
+            p2_damage = int(info[i+2].split("=")[1])
+
+def create_action_list(deck1, deck2):
+    global action_list
+    p1 = [p1_deck[0], p1_deck[1], p1_deck[2]]
+    p2 = [p2_deck[0], p2_deck[1], p2_deck[2]]
+    action_list = []
+    action_list += ["none"]
+    for player in range(1,3):
+        for card in range(1,4):
+            action_list += ["p"+str(player)+"_draw_p"+str(player)+"c"+str(card)]
+    for player in [("p1","p2"), ("p2","p1")]:
+        for target in ["", "c1", "c2", "c3"]:
+            action_list += [player[0]+"_hero_attack_" + player[1] + target]
+    cards = p1+p2
+    for i in range(len(cards)):
+        card_string = "p1"
+        opponent = "p2"
+        if i > 2:
+            card_string = "p2"
+            opponent = "p1"
+        card_string += "c" + str(i%3 + 1)
+        if cards[i] in ["G", "K", "R"]:
+            for target in ["", "c1", "c2", "c3"]:
+                action_list += [card_string + "_attack_" + opponent + target]
+        elif cards[i] == "P":
+            for target in ["_"+card_string[:2], "_c1", "_c2", "_c3"]:
+                action_list += [card_string + "_heal" + target]
+        elif cards[i] == "A":
+            action_list += [card_string + "_attacks_all","not_used","not_used","not_used"]
+    action_list += ["next_turn"]
+
+# Takes an action string, returns string of actions ID (it's index in the action list)
+def find_action_id(action):
+    for i in range(len(action_list)):
+        if action == action_list[i]:
+            return str(i)
+
+# Take a transition file and adds all relevant transitions into a dictionary, returns ordered dictionary
+def populate_transitions(file):
+    transitions = {}
+    for line in open(file+".tra","r").readlines()[1:]:           # First line desribes columns - ignore.
+        detail = line.split()
+        if "player_" + str(t) + "_turn" == detail[4]:
+            transitions[detail[0]] = [detail[2],detail[4]]
+    return transitions
+
+# Take a state file and adds all relevant states into a dictionary, returns dictionary
+def populate_states(file):
+    states = {}
+    for line in open(file+".sta","r").readlines()[1:]:
+        state_value_pair = line.split(":")
+        values = state_value_pair[1][1:-2].split(",")
+        if values[-1] == "0" and values[-2] == str(t):
+            states[state_value_pair[0]] = values
+    return collections.OrderedDict(sorted(states.items(), key=lambda t: t[1]))
+
+# Takes in a list describing local state and prints corresponding guard
+def print_guard(values):
+    print "\t[player_" + str(t) + "_turn]\tattack = 0 & turn_clock = " + str(t) + " &",
+    variables = ["p1", "p2", "p1c1", "p1c2", "p1c3", "p2c1", "p2c2", "p2c3"]
+    for i in range(len(variables)):
+        print variables[i] + " = " + values[i],
+        if i < 7 and i != 3:
+            print "&",
+        elif i == 3:
+            print "&\n\t\t\t",
+        else:
+            print "->"
+
+# Takes a state_id and prints the command corresponding to action taken from that state in adv
+def print_command(state_id):
+    dec_state = transitions[state_id][0]
+    comm_str = transitions[dec_state][1]
+    print "\t\t\t1\t: (action' = " + find_action_id(comm_str) + ")"
+
+def run(deck1, deck2, file, team):
+    global t, transitions, states
+    t = team
+    read_player_info(deck1, deck2)
+    create_action_list(deck1, deck2)
+    #transitions = populate_transitions(file)
+    states = populate_states(file)
+    print "// Educated Strategy for player " + str(t)
+    for state in states:                # For every relevant state
+        print_guard(states[state])          # Print the guard for said state"""
+        print_command(state)
+    print
+
+run(1,3,"tmp",1)
--- prefix.py
+++ prefix.py
@@ -1,3 +1,4 @@
+import sys
 # Reads from info.txt and sets global values on players
 def read_player_info(deck1, deck2):
     global p1_damage, p1_health, p1_deck, p2_damage, p2_health, p2_deck, info
@@ -33,7 +34,7 @@ def read_card_info(card, player, index):
             print "const int p"+player+"c"+index+"_dmg =",                  # print damage
             print info[i+2].split("=")[1][:-1]+";"
             print "const double p"+player+"c"+index+"_acc =",                # print accuracy
-            print info[i+3].split("=")[1]+";"
+            print info[i+3].split("=")[1][:-1]+";"
 
 # Print constant values in file header
 def display_header(deck1, deck2):
@@ -60,9 +61,10 @@ def find_max_card_health():
     return max_health
 
 # Print local variables in module header
-def display_local_variables(deck1, deck2):
+def display_local_variables_with_init(deck1, deck2):
     max_card_health = find_max_card_health()
-    print "\tp1 : [0..p1_hea] init p1_hea;\t//player 1 hero health"         # Might need changing if Ps health differs
+    print "\t//Single initial state (init calls included)"
+    print "\tp1 : [0..p1_hea] init p1_hea;\t//player 1 hero health"
     print "\tp2 : [0..p2_hea] init p2_hea;\t//player 2 hero health"
     for card in ["p1c1","p1c2","p1c3","p2c1","p2c2","p2c3"]:
         print "\t" + card + " : [-1.." + str(max_card_health) + "] init -1;",
@@ -74,13 +76,83 @@ def display_local_variables(deck1, deck2):
     print "\taction : [0..39] init 0;\t//What action has been chosen, 0 = action decision"
     print
 
+# For models with multiple initial states
+def display_local_variables_no_init(deck1, deck2):
+    max_card_health = find_max_card_health()
+    print "\t//Multiple initial states (init calls omitted)"
+    print "\tp1 : [0..p1_hea];\t//player 1 hero health"
+    print "\tp2 : [0..p2_hea];\t//player 2 hero health"
+    for card in ["p1c1","p1c2","p1c3","p2c1","p2c2","p2c3"]:
+        print "\t" + card + " : [-1.." + str(max_card_health) + "];",
+        if card == "p1c1":
+            print "\t//card status/health, -1 = card in hand, 0 = card dead."
+        else:
+            print
+    print "\tturn_clock : [0..2];\t//Whose turn is it, 0 = flip coin"
+    print "\taction : [0..39];\t//What action has been chosen, 0 = action decision"
+    print
+
+def create_action_list(deck1, deck2):
+    global action_list
+    p1 = [p1_deck[0], p1_deck[1], p1_deck[2]]
+    p2 = [p2_deck[0], p2_deck[1], p2_deck[2]]
+    action_list = []
+    action_list += ["none"]
+    for player in range(1,3):
+        for card in range(1,4):
+            action_list += ["p"+str(player)+"_draw_p"+str(player)+"c"+str(card)]
+    for player in [("p1","p2"), ("p2","p1")]:
+        for target in ["", "c1", "c2", "c3"]:
+            action_list += [player[0]+"_hero_attack_" + player[1] + target]
+    cards = p1+p2
+    for i in range(len(cards)):
+        card_string = "p1"
+        opponent = "p2"
+        if i > 2:
+            card_string = "p2"
+            opponent = "p1"
+        card_string += "c" + str(i%3 + 1)
+        if cards[i] in ["G", "K", "R"]:
+            for target in ["", "c1", "c2", "c3"]:
+                action_list += [card_string + "_attack_" + opponent + target]
+        elif cards[i] == "P":
+            for target in ["_"+card_string[:2], "_c1", "_c2", "_c3"]:
+                action_list += [card_string + "_heal" + target]
+        elif cards[i] == "A":
+            action_list += [card_string + "_attacks_all","not_used","not_used","not_used"]
+    action_list += ["next_turn"]
+
+def display_smg_player_descriptions():
+    print "\nplayer p1\n\t[player_1_turn]\nendplayer\n"
+    print "player p2\n\t[player_2_turn]\nendplayer\n"
+    print "player system"
+    printed = 0
+    action_list[0] = "flip_coin"
+    for i in range(len(action_list)):
+        if action_list[i] == "not_used":
+            continue
+        if printed%5 == 0:
+            print "\t",
+        print "[" + action_list[i] + "],",
+        if (printed+1)%5 == 0:
+            print "\n",
+        printed += 1
+    print "\nendplayer"
+
+
 # Main run method. Takes 2 ints for deck numbers of P1 and P2 then model name ("mdp"/"smg" etc)
-def run(deck1, deck2, model):
+def run(deck1, deck2, model, multiple_initial_states):
     print model
     read_player_info(deck1, deck2)
     display_header(deck1, deck2)
     if model == "smg":
-        print "\n#########\nNEED TO IMPLEMENT PLAYER DESCRIPTIONS\n#########"
-    print "\nmodule game\n"
-    display_local_variables(deck1, deck2)
-    print "\t[flip_coin] turn_clock = 0 ->\n\t\t0.5 : (turn_clock' = 1) + 0.5 : (turn_clock' = 2);"
+        create_action_list(deck1, deck2)
+        display_smg_player_descriptions()
+    if multiple_initial_states == 1 or multiple_initial_states == "1":
+        print "\nmodule game\n"
+        display_local_variables_no_init(deck1, deck2)
+        print "\t[flip_coin] turn_clock = 0 ->\n\t\t0.5 : (turn_clock' = 1) + 0.5 : (turn_clock' = 2);"
+    else:
+        print "\nmodule game\n"
+        display_local_variables_with_init(deck1, deck2)
+        print "\t[flip_coin] turn_clock = 0 ->\n\t\t0.5 : (turn_clock' = 1) + 0.5 : (turn_clock' = 2);"
