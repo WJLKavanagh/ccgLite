0262c53fe4df865c0f7e01df131c5f852fb26c24
William_Kavanagh <bkav93@gmail.com>
2018-10-09 14:41:30 +0100

    strat_disp fix

--- strat_disparity.py
+++ strat_disparity.py
@@ -3,13 +3,13 @@ import os
 file_max = int(input("What's the highest file number we care about? "))
 
 for i in range(file_max, 0, -2):
-    for j in range(file_max-2, 0, -2):
+    for j in range(i-2, -1, -2):
         print "Comparing " + str(i) + " to " + str(j) + ":"
         os.system("diff -U 0 adv_strat_"+str(i)+".txt adv_strat_"+str(j)+".txt | grep  ^@ | wc -l")
     print
 
-for j in range(file_max-1, 0, -2):
-    for j in range(file_max-3, 0, -2):
+for i in range(file_max-1, 0, -2):
+    for j in range(i-2, -1, -2):
         print "Comparing " + str(i) + " to " + str(j) + ":"
         os.system("diff -U 0 adv_strat_"+str(i)+".txt adv_strat_"+str(j)+".txt | grep  ^@ | wc -l")
     print
