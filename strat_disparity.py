1a6e8893b4a7cb0c6a9874fa81b01cb6f14711b6
William_Kavanagh <bkav93@gmail.com>
2018-10-09 14:52:37 +0100

    strat_disp redesign

--- strat_disparity.py
+++ strat_disparity.py
@@ -1,15 +1,16 @@
-import os
+import subprocess
+
+# Calculating number of states..
+num_states = 5*5*5*5*5*5*5*5
 
 file_max = int(input("What's the highest file number we care about? "))
 
 for i in range(file_max, 0, -2):
     for j in range(i-2, -1, -2):
-        print "Comparing " + str(i) + " to " + str(j) + ":"
-        os.system("diff -U 0 adv_strat_"+str(i)+".txt adv_strat_"+str(j)+".txt | grep  ^@ | wc -l")
-    print
+        res = int(subprocess.check_output("diff -U 0 adv_strat_"+str(i)+".txt adv_strat_"+str(j)+".txt | grep  ^@ | wc -l", shell=True))
+        print "Comparing " + str(i) + " to " + str(j) + ": " + str(res) + ", output differs in " + str(res/num_states * 100) + "% of states"
 
 for i in range(file_max-1, 0, -2):
     for j in range(i-2, -1, -2):
-        print "Comparing " + str(i) + " to " + str(j) + ":"
-        os.system("diff -U 0 adv_strat_"+str(i)+".txt adv_strat_"+str(j)+".txt | grep  ^@ | wc -l")
-    print
+        res = int(subprocess.check_output("diff -U 0 adv_strat_"+str(i)+".txt adv_strat_"+str(j)+".txt | grep  ^@ | wc -l", shell=True))
+        print "Comparing " + str(i) + " to " + str(j) + ": " + str(res) + ", output differs in " + str(res/num_states * 100) + "% of states"
